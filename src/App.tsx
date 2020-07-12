f392d878114b94bb8de026755d8389a3688b76dc
____marcell <0000marcell@gmail.com>
2020-07-12 22:39:51 +0000

    getting-started finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './App.css';
+import { Machine, interpret } from 'xstate';
+
+const promiseMachine = Machine({
+  id: 'promise',
+  initial: 'pending',
+  states: {
+    pending: {
+      on: {
+        RESOLVE: 'resolved',
+        REJECT: 'rejected'
+      }
+    },
+    resolved: {
+      type: 'final'
+    },
+    rejected: {
+      type: 'final'
+    }
+  }
+});
+
+const promiseService = interpret(promiseMachine).onTransition(state =>
+  console.log(state.value)
+);
 
 function App() {
+  promiseService.start();
+  // => 'pending'
+  promiseService.send('RESOLVE');
   return (
     <div className="App">
-      <h1>xstate assign</h1>
+      <h1>Getting Started</h1>
     </div>
   );
 }
