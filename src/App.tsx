6e46933c6fbac9ada9b3defed9cff3a6e71fac53
____marcell <0000marcell@gmail.com>
2020-07-12 22:27:17 +0000

    start following the tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <Header /> 
+      <h1>xstate assign</h1>
     </div>
   );
 }
